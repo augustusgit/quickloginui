8ceea920d0cc774bd3ea49d44bc4e43469bc14a4
Augustus Irechukwu <augustusnwangwu@gmail.com>
2021-06-12 12:33:13 +0100

    first commit

---
+++ app/src/main/java/com/quickqart/test/Signup_form.java
@@ -0,0 +1,16 @@
+package com.quickqart.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_form);
+//        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("Signup Form");
+    }
+}
---
+++ app/src/main/java/com/quickqart/test/login_form.java
@@ -0,0 +1,22 @@
+package com.quickqart.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class login_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+        getSupportActionBar().hide();
+    }
+
+
+    public void btn_signupForm(View view) {
+        startActivity(new Intent(getApplicationContext(), Signup_form.class));
+    }
+}
